322a485d49b9a3eb67ab04094c78a938097ed1de
sujaykumar <82111261+sujaykumar007@users.noreply.github.com>
2025-09-22 22:12:54 +0530

    change logo sqare

--- app/layout.tsx
+++ app/layout.tsx
@@ -105,7 +105,8 @@ export default function RootLayout({
     "@type": "Organization",
     name: "Hacfy",
     url: "https://hacfy.com",
-    logo: "https://hacfy.com/logo.png",
+    // logo: "https://hacfy.com/logo.png",
+    "logo": "https://hacfy.com/logo-square.png",
     sameAs: [
       "https://www.linkedin.com/company/hacfy",
       "https://x.com/hacfy",
